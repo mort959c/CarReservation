00e2f5156889cb89d7c08eadb1f646d8827b08e1
mort959c <morten.graabek@gmail.com>
2018-03-22 13:08:36 +0100

    FirstCommit

---
+++ CarReservationMorten/CarReservationMorten/Classes.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CarReservationMorten
+{
+    public class Employee
+    {
+        private string EmailDomain { get; set; } = "@ssa-nordjylland";
+        private string FirstName { get; set; }
+        private int Id { get; set; }
+        private string Initials { get; set; }
+        private string LastName { get; set; }
+        private string PhoneNumber { get; set; }
+
+        public Employee(int id, string firstName, string lastName, string phoneNumber, string initials)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.PhoneNumber = phoneNumber;
+            this.Initials = initials; 
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Initials} - {this.FirstName} - {this.LastName} - {this.PhoneNumber} - {this.Initials + this.EmailDomain}";
+        }
+    }
+
+    public class Car
+    {
+        private int Id { get; set; }
+        private bool IsAvailable { get; set; }
+        private string LicensePlate { get; set; }
+        private string Make { get; set; }
+        private string Model { get; set; }
+        private int ProductionYear { get; set; }
+
+        public Car(int id, string licensePlate, int productionYear, string make, string model, bool isAvailable)
+        {
+            this.Id = id;
+            this.LicensePlate = licensePlate;
+            this.ProductionYear = productionYear;
+            this.Make = make;
+            this.Model = model;
+            this.IsAvailable = isAvailable;
+        }
+
+        public Car(string licensePlate, int productionYear, string make, string model)
+        {
+            this.LicensePlate = licensePlate;
+            this.ProductionYear = productionYear;
+            this.Make = make;
+            this.Model = model;
+        }
+
+        public bool getBookingStatus()
+        {
+            return this.IsAvailable;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Make} - {this.Model} - {this.LicensePlate} - {this.ProductionYear} - {this.IsAvailable.ToString()}";
+        }
+    }
+
+    public class Access
+    {
+        //sql connection string to the database
+        SqlConnection myConnection = new SqlConnection(@"Data Source = CV-PC-T-41\SQLEXPRESS; Initial Catalog = CarReservation; Integrated Security = True");
+        SqlCommand myCommand = null;
+        SqlDataReader myReader = null;
+
+        // gets all the employees from the database
+        public List<Employee> getAllEmployees()
+        {
+            //List that gets filled with all the employees before being returned
+            List<Employee> employees = new List<Employee>();
+
+            //simple sql command. gets all columns from table
+            string sql = @"SELECT * FROM Employee";
+            myCommand = new SqlCommand(sql, myConnection);
+
+            try
+            {
+                myConnection.Open();
+                myReader = myCommand.ExecuteReader();
+
+                while (myReader.Read())
+                {
+                    // creates an employee object with the data gotten from the database
+                    Employee e = new Employee(Convert.ToInt32(myReader["ID"]),
+                        myReader["FirstName"].ToString(),
+                        myReader["LastName"].ToString(),
+                        myReader["PhoneNumber"].ToString(),
+                        myReader["Initials"].ToString());
+
+                    //adds the newly create employee to the list
+                    employees.Add(e);
+                }
+
+                return employees;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+        }
+
+        public List<Car> getAllCars()
+        {
+            List<Car> cars = new List<Car>();
+
+            string sql = @"SELECT * FROM Cars";
+            myCommand = new SqlCommand(sql, myConnection);
+
+            try
+            {
+                myConnection.Open();
+                myReader = myCommand.ExecuteReader();
+
+                while (myReader.Read())
+                {
+                    // 0 equals occupied 1 equals free
+                    bool isavailable;
+                    if (Convert.ToInt32(myReader["IsAvailable"]) == 1) isavailable = true;
+                    else isavailable = false;
+
+                        Car c = new Car(Convert.ToInt32(myReader["ID"]),
+                        myReader["LicensePlate"].ToString(),
+                        Convert.ToInt32(myReader["ProductionYear"]),
+                        myReader["Make"].ToString(),
+                        myReader["Model"].ToString(),
+                        isavailable);
+
+                    cars.Add(c);
+                }
+                return cars;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+        }
+    }
+}
---
+++ CarReservationMorten/CarReservationMorten/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarReservationMorten
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Employee> employees = new List<Employee>();
+        List<Car> cars = new List<Car>();
+        Access a = new Access();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            fillCombobox();
+        }
+
+        // fills the cboEmployee with all the employees in the database
+        public void fillCombobox()
+        {
+            employees.Clear();
+            //instance of the access class used the call the methods from that class
+            
+            try
+            {
+                employees = a.getAllEmployees();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("noget gik galt det var ikke muligt at indlæse ansatte fra databasen \n\n" + ex);
+            }
+
+            cboEmployee.ItemsSource = employees;
+            //preselects a item, to insure that a "legit" value is always chosen
+            cboEmployee.SelectedIndex = 0;
+        }
+
+        // Fills the datagridCars with all the cars in the databse
+        public void fillDataGridCars()
+        {
+
+        }
+
+        private void btnNewCar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //event handler for btnShowAllCars and btnShowFreeCars
+        private void AllAndFreeCarsBtns_Clicked(object sender, RoutedEventArgs e)
+        {
+            //if the car list contains anything
+            if (!cars.Any())
+            {
+                try
+                {
+                    cars = a.getAllCars();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("noget gik galt da der blev forsøgt at hente biler fra databasen \n\n" + ex);
+                }
+            }
+
+            if (listBoxCars.Items.Count != 0)
+            {
+                //clears the datagrid
+                listBoxCars.Items.Clear();
+            }
+
+            // if show all cars was pressed all cars will be showed
+            if (sender == btnShowAllCars)
+            {
+                foreach (Car c in cars)
+                {
+                    listBoxCars.Items.Add(c);
+                }
+            }
+            // if show free cars was pressed all the cars that is free will be showed
+            else
+            {
+                // checks each car in the list to see if its free if it is free it will get added to the datagrid
+                foreach (Car c in cars)
+                {
+                    if (c.getBookingStatus())
+                    {
+                        listBoxCars.Items.Add(c);
+                    }
+                }
+            }
+        }
+
+        private void cboEmployee_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach (Employee em in employees)
+            {
+                
+            }
+        }
+    }
+}
